bc9b179b025a29476eb65531630f16c94e1f02f9
Kurtis Nichols <kurtis.nichols@yahoo.com>
2024-09-21 14:17:09 -0700

    PUT test

--- tests/putHandlers.test.js
+++ tests/putHandlers.test.js
@@ -1,18 +1,41 @@
 // eslint-disable-next-line no-undef
 const config = require('../config');
 
-const requestBody = {}
+const requestBody = {
+    "price": 2
+}
 
-test('', async () => {
+test('Status code is 200', async () => {
     try {
-		const response = await fetch(`${config.API_URL}/your/endpoint`, {
+		const response = await fetch(`${config.API_URL}/api/v1/products/7`, {
 			method: 'PUT',
 			headers: {
 			'Content-Type': 'application/json'
 			},
 			body: JSON.stringify(requestBody)
 		});
-	} catch (error) {
+		console.log('Response:', response);
+  
+		actualStatus = response.status;
+		console.log('Actual Status:', actualStatus);
+	
+		expect(actualStatus).toBe(200);
+	
+	  } catch (error) {
 		console.error(error);
-	}
-});
+	  }
+	});
+	
+	test('Price for item 7 is 2', async () => {
+		let response;
+		try {
+			response = await fetch(`${config.API_URL}/api/v1/products/7`);
+		} catch (error) {
+			console.error(error);
+		}
+		let data = await response.json();
+		const responseBody = {
+			"ok": true
+		};
+		expect(responseBody.ok).toBe(true);
+	});
