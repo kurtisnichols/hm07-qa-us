0431e7501147798b51058fa72f8d3616713d6b09
Kurtis Nichols <kurtis.nichols@yahoo.com>
2024-09-21 14:16:45 -0700

    DELETE test

--- tests/deleteHandlers.test.js
+++ tests/deleteHandlers.test.js
@@ -1,12 +1,33 @@
 // eslint-disable-next-line no-undef
 const config = require('../config');
 
-test('', async () => {
+test('Status should be 200', async () => {
     try {
-		const response = await fetch(`${config.API_URL}/your/endpoint`, {
+		const response = await fetch(`${config.API_URL}/api/v1/kits/6`, {
 			method: 'DELETE',
 		});
+		console.log('Response:', response);
+  
+		actualStatus = response.status;
+		console.log('Actual Status:', actualStatus);
+	
+		expect(actualStatus).toBe(200);
+
 	} catch (error) {
 		console.error(error);
 	}
+});
+
+test('Kit has been deleted', async () => {
+	let response;
+    try {
+        response = await fetch(`${config.API_URL}/api/v1/kits/6`);
+    } catch (error) {
+		console.error(error);
+	}
+	let data = await response.json();
+	const responseBody = {
+		"ok": true
+	};
+	expect(responseBody.ok).toBe(true);
 });
