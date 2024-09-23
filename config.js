5a858a4daf60bd212a20876b7e5813517cc32c9f
Kurtis Nichols <kurtis.nichols@yahoo.com>
2024-09-22 20:31:01 -0700

    Updated URL

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://cnt-1c61a40f-f5b6-40de-a42c-f4da15140fd8.containerhub.tripleten-services.com"
+    API_URL: "https://cnt-f602c2be-d4a0-413d-8a3d-e04779e46946.containerhub.tripleten-services.com"
 }
