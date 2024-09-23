b7219dc485399756053a6b7ed01e3d216f57ebf3
Kurtis Nichols <kurtis.nichols@yahoo.com>
2024-09-22 20:31:57 -0700

    Updated test titles and removed console.log()

--- tests/deleteHandlers.test.js
+++ tests/deleteHandlers.test.js
@@ -1,15 +1,13 @@
 // eslint-disable-next-line no-undef
 const config = require('../config');
 
-test('Status should be 200', async () => {
+test('Should be 200 status code', async () => {
     try {
 		const response = await fetch(`${config.API_URL}/api/v1/kits/6`, {
 			method: 'DELETE',
 		});
-		console.log('Response:', response);
   
 		actualStatus = response.status;
-		console.log('Actual Status:', actualStatus);
 	
 		expect(actualStatus).toBe(200);
 
@@ -18,7 +16,7 @@ test('Status should be 200', async () => {
 	}
 });
 
-test('Kit has been deleted', async () => {
+test('Should delete the kit', async () => {
 	let response;
     try {
         response = await fetch(`${config.API_URL}/api/v1/kits/6`);
